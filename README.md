1b93948f5c13ce2549cd4c98dbfc8294d8fd35ca
Richard Pope <richardpope@MacBook-Pro-2.local>
2016-03-26 12:28:24 +0000

    pre re-org

---
+++ README.md
@@ -0,0 +1,2 @@
+using older version for now:
+http://foundation.zurb.com/sites/docs/v/5.5.3/javascript.html
--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
+from flask.ext.mongoengine import MongoEngine
 import jinja2
 import os
+import forms
+import models
 
 app = Flask(__name__)
-app.debug = True
+app.config.from_object(os.environ.get('SETTINGS', 'config.DevelopmentConfig'))
+db = MongoEngine(app)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+@app.route("/", methods=['GET', 'POST'])
+def condition():
+
+    condition_statement = app.config['CONDITION_STATEMENT']
+    form = forms.Commitment()
+
+    if request.method == 'POST' and form.validate():
+        commitment = models.Commitment(app.config['DATABASE_ENCRYPTION_KEY'])
+        commitment.name = form.data['name']
+        commitment.mobile_number = form.data['mobile_number']
+        commitment.save()
+
+        return redirect(url_for('committed'))
+
+    return render_template('condition.html', condition_statement=condition_statement, form=form)
+
+@app.route("/done")
+def committed():
+    commitment_count = models.Commitment.objects.count()
+    return render_template('committed.html', commitment_count=commitment_count)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
