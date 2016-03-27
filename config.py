b3ee072765dbedaaeb9738c6207d2df11e68b80b
Richard Pope <richardpope@MacBook-Pro-2.local>
2016-03-27 18:56:55 +0100

    live config

--- config.py
+++ config.py
@@ -2,9 +2,18 @@ import os
 
 class Config(object):
     DEBUG = False
-    # MONGODB_DB = os.environ.get('MONGODB_DB', None)
-    # MONGODB_HOST = os.environ.get('MONGODB_HOST', None)
-    # SECRET_KEY = os.environ.get('SECRET_KEY', None)
+    MONGODB_DB = os.environ.get('MONGODB_DB', None)
+    MONGODB_HOST = os.environ.get('MONGODB_HOST', None)
+    SECRET_KEY = os.environ.get('SECRET_KEY', None)
+    DATABASE_ENCRYPTION_KEY = os.environ.get('DATABASE_ENCRYPTION_KEY', None)
+    CONDITION_TARGET = os.environ.get('CONDITION_TARGET', None)
+    CONDITION_STATEMENT = os.environ.get('CONDITION_STATEMENT', None)
+    TWILLIO_SID = os.environ.get('TWILLIO_SID', None)
+    TWILLIO_AUTH_TOKEN = os.environ.get('TWILLIO_AUTH_TOKEN', None)
+    TWILLIO_PHONE_NUMBER = os.environ.get('TWILLIO_PHONE_NUMBER', None)
+    CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', None)
+    CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', None)
+    CELERY_TIMEZONE = os.environ.get('CELERY_TIMEZONE', None)
 
 class DevelopmentConfig(Config):
     DEBUG = True
